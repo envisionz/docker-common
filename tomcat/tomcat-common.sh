ec7abf4ab7721d125bd2ced5db0b718d5a7891e0
shermp <14854761+shermp@users.noreply.github.com>
2021-09-07 09:59:09 +1200

    Add set_app_path function

--- tomcat/tomcat-common.sh
+++ tomcat/tomcat-common.sh
@@ -9,3 +9,24 @@ strip_url_path()
     printf "%s" "$path"
     return 0
 }
+
+set_app_path()
+{
+    local orig_path="$1"
+    local new_path="$2"
+
+    local orig_dir="${CATALINA_HOME}/webapps/${orig_path}"
+    if [ -z "$new_path" ]; then
+        # Set as the ROOT webapp
+        local newdir="${CATALINA_HOME}/webapps/ROOT"
+        [ ! -d "$newdir" ] && mv -T "$orig_dir" "$newdir"
+        printf "%s" "$newdir"
+    else
+        # In Tomcat, use '#' in webapp filename to create path separator
+        local newpath="${new_path//\//#}"
+        local newdir="${CATALINA_HOME}/webapps/${newpath}"
+        [ ! -d "$newdir" ] && mv -- "$orig_dir" "$newdir"
+        printf "%s" "$newdir"
+    fi
+    exit 0
+}
