5eb2ea69cdaed13e88fd7e310167ec429aab0be5
shermp <14854761+shermp@users.noreply.github.com>
2021-09-07 10:33:35 +1200

    Add set_connector_proxy function

--- tomcat/tomcat-common.sh
+++ tomcat/tomcat-common.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+tc_print()
+{
+    printf "%s\n" "$1"
+}
+
+xml_add_update_element()
+{
+    local xpath_root="$1"
+    local element="$2"
+    local value="$3"
+    local xml_file="$4"
+    xmlstarlet ed -P -S -L \
+        -s "$xpath_root" -t elem -n "$element" -v '' \
+        -d "${xpath_root}/${element}[position() != 1]" \
+        -u "${xpath_root}/${element}" -v "$value" \
+        "$xml_file"
+}
+
+xml_add_update_attr()
+{
+    local xpath_el="$1"
+    local attr_name="$2"
+    local attr_val="$3"
+    local xml_file="$4"
+
+    xmlstarlet ed -P -S -L \
+        -u "${xpath_el}/@${attr_name}" -v "$attr_val" \
+        -i "${xpath_el}[not(@${attr_name})]" -t attr -n "$attr_name" -v "$attr_val" \
+        "$xml_file"
+}
+
 # Strip leading and trailing '/' from path
 strip_url_path()
 {
@@ -29,4 +60,36 @@ set_app_path()
         printf "%s" "$newdir"
     fi
     exit 0
+}
+
+# Setup tomcat connector when behind reverse proxy
+set_connector_proxy()
+{
+    local rev_proxy_domain="$1"
+    local rev_proxy_proto="$2"
+
+    [ -z "$rev_proxy_domain" ] && tc_print "Reverse proxy domain not set" && return 1
+    [ -z "$rev_proxy_proto" ] && tc_print "Reverse proxy protocol not set" && return 1
+
+    local secure="false"
+    local port="80"
+    if [ "$rev_proxy_proto" = "https" ]; then
+        secure="true"
+        port="443"
+    fi
+
+    # Add an appropriate connector in the Tomcat server configuration
+    local connector_xpath="/Server/Service[@name=\"Catalina\"]/Connector[@port=\"8080\"]"
+    local server_xml="${CATALINA_HOME}/conf/server.xml"
+
+    xmlstarlet ed -P -S -L \
+        -d "${connector_xpath}/@redirectPort" \
+        "$server_xml"
+    
+    xml_add_update_attr "$connector_xpath" "proxyName" "$proxy_domain" "$server_xml"
+    xml_add_update_attr "$connector_xpath" "proxyPort" "$port" "$server_xml"
+    xml_add_update_attr "$connector_xpath" "scheme" "$proxy_proto" "$server_xml"
+    xml_add_update_attr "$connector_xpath" "secure" "$secure" "$server_xml"
+
+    return 0
 }
